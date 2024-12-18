a28a0072c0e2203ca2a12bab4cebf8fc7a7d071c
liarles <62631264+liarles@users.noreply.github.com>
2024-12-18 18:11:15 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,52 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+
+{
+
+        int count = 0;
+
+        float sum = 0;
+
+        float sum_of_square = 0;
+
+        string textline;
+
+        ifstream source("score.txt");
+
+        while (getline(source, textline))
+
+        {
+            float num = atof(textline.c_str());
+                sum += num;
+
+                sum_of_square += num*num;
+
+                count++;
+
+        }
+        source.close();
+        
+        float mean = sum / count;
+        float standart = sqrt((sum_of_square/count)-pow(mean,2));
+
+        cout << "Number of data = " << count << "\n";
+
+        cout << setprecision(3);
+
+        cout << "Mean = " << mean << "\n";
+
+        cout << "Standard deviation = " << standart;
+        
+        return 0;
+
+}
+
